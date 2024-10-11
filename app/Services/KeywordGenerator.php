92c4ab5c29ddcf1202362ef4b1726f8a635b40db
Eugene Bachura <eugenebachura@gmail.com>
2024-10-11 19:19:04 +0200

    Create KeywordGenerator service
    Install php-text-analysis and create KeywordGenerator service

---
+++ app/Services/KeywordGenerator.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Services;
+
+use TextAnalysis\Tokenizers\GeneralTokenizer;
+use TextAnalysis\Analysis\FreqDist;
+use TextAnalysis\Filters\StopWordsFilter;
+use TextAnalysis\Utilities\Text;
+use TextAnalysis\Documents\TokensDocument;
+use TextAnalysis\Stemmer\PorterStemmer;
+
+class KeywordGenerator
+{
+    /**
+     * Генерирует ключевые слова на основе текста
+     *
+     * @param string $content
+     * @param string $language
+     * @param int $limit
+     * @return string
+     */
+    public function generate(string $content, string $language = 'en', int $limit = 10): string
+    {
+        // Убираем HTML-теги и специальные символы
+        $cleanedContent = strip_tags($content);
+
+        // Приводим текст к нижнему регистру
+        $cleanedContent = mb_strtolower($cleanedContent);
+
+        // Токенизация текста
+        $tokenizer = new GeneralTokenizer();
+        $tokens = $tokenizer->tokenize($cleanedContent);
+
+        // Удаление стоп-слов
+        $stopWords = $this->getStopWords($language);
+        $tokens = array_filter($tokens, function ($token) use ($stopWords) {
+            return !in_array($token, $stopWords) && preg_match('/^\p{L}+$/u', $token);
+        });
+
+        // Получение частоты встречаемости слов
+        $freqDist = new FreqDist($tokens);
+        $mostFrequent = $freqDist->getKeyValuesByFrequency();
+        arsort($mostFrequent);
+
+        // Извлечение топовых ключевых слов
+        $topKeywords = array_slice(array_keys($mostFrequent), 0, $limit);
+
+        // Преобразование массива ключевых слов в строку
+        return implode(', ', $topKeywords);
+    }
+
+    /**
+     * Получает список стоп-слов для заданного языка
+     *
+     * @param string $language
+     * @return array
+     */
+    private function getStopWords(string $language): array
+    {
+        $stopWords = [];
+
+        switch ($language) {
+            case 'ru':
+                $stopWords = ['и', 'в', 'не', 'на', 'что', 'я', 'с', 'со', 'как', 'а', 'то'];
+                break;
+            case 'pl':
+                $stopWords = ['i', 'w', 'nie', 'na', 'co', 'ja', 'z', 'że', 'to', 'a'];
+                break;
+            default:
+                // Английские стоп-слова по умолчанию
+                $stopWords = ['the', 'and', 'is', 'in', 'at', 'of', 'a', 'to', 'it', 'that'];
+                break;
+        }
+
+        return $stopWords;
+    }
+}
