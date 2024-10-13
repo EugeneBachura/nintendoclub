8696a706d2aa695f8f5083de25463bd07bfba614
Eugene Bachura <eugenebachura@gmail.com>
2024-10-13 16:16:56 +0200

    Refactor: Remove php-text-analysis and enhance keyword generation method
    Refactor: Removed php-text-analysis and improved keyword extraction with custom logic and expanded stop words list

--- app/Services/KeywordGenerator.php
+++ app/Services/KeywordGenerator.php
@@ -2,50 +2,46 @@
 
 namespace App\Services;
 
-use TextAnalysis\Analysis\FreqDist;
-use TextAnalysis\Filters\StopWordsFilter;
-
 class KeywordGenerator
 {
     /**
-     * Генерирует ключевые слова на основе текста
+     * Генерирует ключевые слова на основе текста.
      *
      * @param string $content
      * @param string $language
      * @param int $limit
      * @return string
      */
-    public function generate(string $content, string $language = 'en', int $limit = 10): string
+    public function generate(string $content, string $language = 'en', int $limit = 7): string
     {
-        // Убираем HTML-теги и специальные символы
+        // Удаляем HTML-теги и специальные символы
         $cleanedContent = strip_tags($content);
-
-        // Приводим текст к нижнему регистру
         $cleanedContent = mb_strtolower($cleanedContent);
 
-        // Разбиваем текст на токены (слова) с использованием встроенных функций PHP
-        $tokens = preg_split('/\s+/', $cleanedContent, -1, PREG_SPLIT_NO_EMPTY);
+        // Удаляем знаки пунктуации и специальные символы
+        $cleanedContent = preg_replace('/[^\p{L}\s]+/u', '', $cleanedContent);
 
-        // Удаление стоп-слов
+        // Разбиваем на слова
+        $words = preg_split('/\s+/', $cleanedContent, -1, PREG_SPLIT_NO_EMPTY);
+
+        // Удаляем стоп-слова
         $stopWords = $this->getStopWords($language);
-        $tokens = array_filter($tokens, function ($token) use ($stopWords) {
-            return !in_array($token, $stopWords) && preg_match('/^\p{L}+$/u', $token);
-        });
+        $keywords = array_diff($words, $stopWords);
 
-        // Получение частоты встречаемости слов
-        $freqDist = new FreqDist($tokens);
-        $mostFrequent = $freqDist->getKeyValuesByFrequency();
-        arsort($mostFrequent);
+        // Подсчитываем частоту слов
+        $frequency = array_count_values($keywords);
 
-        // Извлечение топовых ключевых слов
-        $topKeywords = array_slice(array_keys($mostFrequent), 0, $limit);
+        // Сортируем по убыванию частоты
+        arsort($frequency);
 
-        // Преобразование массива ключевых слов в строку
+        // Получаем топ ключевых слов
+        $topKeywords = array_slice(array_keys($frequency), 0, $limit);
+
         return implode(', ', $topKeywords);
     }
 
     /**
-     * Получает список стоп-слов для заданного языка
+     * Возвращает массив стоп-слов для указанного языка.
      *
      * @param string $language
      * @return array
@@ -56,14 +52,212 @@ class KeywordGenerator
 
         switch ($language) {
             case 'ru':
-                $stopWords = ['и', 'в', 'не', 'на', 'что', 'я', 'с', 'со', 'как', 'а', 'то'];
+                $stopWords = [
+                    'и',
+                    'в',
+                    'не',
+                    'на',
+                    'что',
+                    'я',
+                    'с',
+                    'со',
+                    'как',
+                    'а',
+                    'то',
+                    'все',
+                    'она',
+                    'так',
+                    'его',
+                    'но',
+                    'да',
+                    'ты',
+                    'к',
+                    'у',
+                    'же',
+                    'вы',
+                    'за',
+                    'бы',
+                    'по',
+                    'только',
+                    'ее',
+                    'мне',
+                    'было',
+                    'вот',
+                    'от',
+                    'меня',
+                    'еще',
+                    'нет',
+                    'о',
+                    'из',
+                    'ему',
+                    'теперь',
+                    'когда',
+                    'даже',
+                    'ну',
+                    'вдруг',
+                    'ли',
+                    'если',
+                    'уже',
+                    'или',
+                    'ни',
+                    'быть',
+                    'был',
+                    'него',
+                    'до',
+                    'вас',
+                    'нибудь',
+                    'опять',
+                    'уж',
+                    'вам',
+                    'ведь',
+                    'там',
+                    'потом',
+                    'себя',
+                    'ничего',
+                    'ей',
+                    'может',
+                    'они',
+                    'тут',
+                    'где',
+                    'есть',
+                    'надо',
+                    'ней',
+                    'для',
+                    'мы',
+                    'тебя',
+                    'их',
+                    'чем',
+                    'была',
+                    'сам',
+                    'чтоб',
+                    'без',
+                    'будто',
+                    'чего',
+                    'раз',
+                    'тоже',
+                    'себе',
+                    'под',
+                    'будет',
+                    'ж',
+                    'тогда',
+                    'кто',
+                    'этот'
+                ];
                 break;
             case 'pl':
-                $stopWords = ['i', 'w', 'nie', 'na', 'co', 'ja', 'z', 'że', 'to', 'a'];
+                $stopWords = [
+                    'i',
+                    'w',
+                    'nie',
+                    'na',
+                    'co',
+                    'ja',
+                    'z',
+                    'że',
+                    'to',
+                    'a',
+                    'się',
+                    'o',
+                    'ale',
+                    'jak',
+                    'jest',
+                    'tak',
+                    'do',
+                    'ty',
+                    'już',
+                    'czy',
+                    'by',
+                    'tylko',
+                    'po',
+                    'dla',
+                    'mnie',
+                    'mój',
+                    'mi',
+                    'jestem',
+                    'ma',
+                    'tego',
+                    'nim',
+                    'tam',
+                    'wszystko',
+                    'są',
+                    'czyli',
+                    'mamy',
+                    'teraz',
+                    'dlaczego',
+                    'może',
+                    'przez',
+                    'go',
+                    'też',
+                    'bardziej',
+                    'jej',
+                    'niż',
+                    'być',
+                    'pan',
+                    'pani',
+                    'dlatego',
+                    'kiedy',
+                    'nawet',
+                    'kto',
+                    'gdzie',
+                    'jeszcze',
+                    'tych'
+                ];
                 break;
             default:
                 // Английские стоп-слова по умолчанию
-                $stopWords = ['the', 'and', 'is', 'in', 'at', 'of', 'a', 'to', 'it', 'that'];
+                $stopWords = [
+                    'the',
+                    'and',
+                    'is',
+                    'in',
+                    'at',
+                    'of',
+                    'a',
+                    'to',
+                    'it',
+                    'that',
+                    'i',
+                    'this',
+                    'for',
+                    'you',
+                    'was',
+                    'with',
+                    'on',
+                    'as',
+                    'but',
+                    'be',
+                    'they',
+                    'he',
+                    'she',
+                    'or',
+                    'which',
+                    'we',
+                    'an',
+                    'by',
+                    'do',
+                    'not',
+                    'are',
+                    'from',
+                    'his',
+                    'her',
+                    'have',
+                    'all',
+                    'my',
+                    'so',
+                    'me',
+                    'up',
+                    'one',
+                    'about',
+                    'who',
+                    'what',
+                    'when',
+                    'where',
+                    'can',
+                    'if',
+                    'would',
+                    'there',
+                    'their'
+                ];
                 break;
         }
 
