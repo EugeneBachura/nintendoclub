4fb3efaae320268f74dff7311bcb7fe0e300c19f
Eugene Bachura <eugenebachura@gmail.com>
2024-10-28 19:34:56 +0100

    Add display limit to Pokemon component

--- resources/views/components/pokemon-component.blade.php
+++ resources/views/components/pokemon-component.blade.php
@@ -1,37 +1,37 @@
-@props(['pokemons' => []])
+@props(['pokemons' => [], 'limit' => 5])
 
 <div class="flex flex-col space-y-2">
-    <div class="text-base">{{ __('profiles.pokemons')}}:</div>
+    <div class="text-base">{{ __('profiles.pokemons') }}:</div>
     <div class="flex flex-wrap space-x-3">
         @php
-            $emptySlots = max(0, 5 - count($pokemons));
+            $displayPokemons = array_slice($pokemons, 0, $limit);
+            $emptySlots = max(0, $limit - count($displayPokemons));
         @endphp
-        
-        @foreach($pokemons as $pokemon)
+
+        @foreach ($pokemons as $pokemon)
             <div x-data="{ open: false }" class="relative">
-                <img src="{{ $pokemon->image_url }}" alt="{{ $pokemon->name }}" width="50" height="50" class="rounded-md cursor-pointer"
-                     @mouseover="open = true" @mouseleave="open = false">
-                     
+                <img src="{{ $pokemon->image_url }}" alt="{{ $pokemon->name }}" width="50" height="50"
+                    class="rounded-md cursor-pointer" @mouseover="open = true" @mouseleave="open = false">
+
                 {{-- Tooltip --}}
-                <div x-show="open" 
-                     class="absolute -top-10 left-full transform -translate-x-1/2 p-2 text-sm bg-white rounded shadow-lg whitespace-nowrap z-10">
+                <div x-show="open"
+                    class="absolute -top-10 left-full transform -translate-x-1/2 p-2 text-sm bg-white rounded shadow-lg whitespace-nowrap z-10">
                     {{ $pokemon->name }}
                 </div>
             </div>
         @endforeach
 
-        @for($i = 0; $i < $emptySlots; $i++)
+        @for ($i = 0; $i < $emptySlots; $i++)
             <div x-data="{ open: false }" class="relative">
                 <div class="w-12 h-12 border-dashed border-2 border-gray-300 rounded-full cursor-pointer"
-                     @mouseover="open = true" @mouseleave="open = false"></div>
-                     
+                    @mouseover="open = true" @mouseleave="open = false"></div>
+
                 {{-- Tooltip for Empty --}}
-                <div x-show="open" 
-                     class="absolute -top-10 left-full transform -translate-x-1/2 p-2 text-sm text-content_text bg-background rounded shadow-lg whitespace-nowrap z-10">
-                     {{__('profiles.empty')}}
+                <div x-show="open"
+                    class="absolute -top-10 left-full transform -translate-x-1/2 p-2 text-sm text-content_text bg-background rounded shadow-lg whitespace-nowrap z-10">
+                    {{ __('profiles.empty') }}
                 </div>
             </div>
         @endfor
     </div>
 </div>
-
