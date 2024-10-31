b1452489f977a5f89d2a60aa4d473d94b57f25d9
Eugene Bachura <eugenebachura@gmail.com>
2024-10-31 11:13:03 +0100

    Refactoring and minor styling changes
    Refactoring and minor styling changes. Update versions page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,11 +6,17 @@
     </x-slot>
 
     <div class="text-color_text">
-        {{-- {{ __("You're logged in") }}, {{$name}} ! --}}
-        
         <div>
-            <h3 class="text-content_text text-lg">{{__('titles.daily_reward')}}</h3>
-            <x-daily-reward collectedDays={{$collectedDays}} collectedToday={{$collectedToday}} />
+            <h3 class="text-content_text text-lg">{{ __('titles.daily_reward') }}</h3>
+            <x-daily-reward collectedDays={{ $collectedDays }} collectedToday={{ $collectedToday }} />
         </div>
+    </div>
+
+    <div class="text-xs opacity-50 flex justify-end w-full pt-2 -mb-4">
+        @if (app()->getLocale() == 'en')
+            <a href="{{ route('updates') }}">Test Ver. 0.3.2</a>
+        @else
+            <a href="@localizedRoute('updates.locale')">Test Ver. 0.3.2</a>
+        @endif
     </div>
 </x-app-layout>
