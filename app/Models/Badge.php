b76e40b4cbecbdb59fffaba15682ac421d3604dd
Eugene Bachura <eugenebachura@gmail.com>
2024-11-03 13:12:48 +0100

    Fix: fixes the display badges

--- app/Models/Badge.php
+++ app/Models/Badge.php
@@ -7,15 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Badge extends Model
 {
-    <?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Badge extends Model
-{
     use HasFactory;
 
     protected $fillable = ['name', 'description', 'icon_url', 'parent_id'];
