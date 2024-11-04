30b3db3fa5ac469aacf8ca27b7f8f4c02bc12f4b
Eugene Bachura <eugenebachura@gmail.com>
2024-11-04 19:31:22 +0100

    Changing language switching logic

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
+
+class SetLocale
+{
+    public function handle($request, Closure $next)
+    {
+        // Определяем поддерживаемые языки
+        $supportedLocales = ['en', 'ru', 'pl'];
+
+        // Получаем язык из параметра ?lang или из сессии/предпочтений пользователя
+        $locale = $request->query('lang', session('locale', Auth::user()->locale ?? config('app.locale')));
+
+        // Устанавливаем локаль только если она поддерживается
+        if (in_array($locale, $supportedLocales)) {
+            App::setLocale($locale);
+            session(['locale' => $locale]);
+
+            if (Auth::check() && Auth::user()->locale !== $locale) {
+                Auth::user()->update(['locale' => $locale]);
+            }
+        }
+
+        return $next($request);
+    }
+}
