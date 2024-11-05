d25eed13a4a19b2cb141171521a925834eeffb83
Eugene Bachura <eugenebachura@gmail.com>
2024-11-05 22:43:10 +0100

    Change routers to new language logic components, profile and store page

--- resources/views/components/favorite-games.blade.php
+++ resources/views/components/favorite-games.blade.php
@@ -15,14 +15,10 @@
                     $locale = app()->getLocale();
                 @endphp
                 @if ($game->logo_url)
-                    @if ($locale == 'en')
-                        <a href="{{ route('game.show', ['alias' => $game->alias]) }}">
-                        @else
-                            <a href="{{ route('game.show.locale', ['alias' => $game->alias, 'locale' => $locale]) }}">
-                    @endif
-                    <div class="bg-cover bg-center w-12 h-12 rounded-md cursor-pointer"
-                        style="background-image: url('{{ asset('storage/' . $game->logo_url) }}');"
-                        @mouseover="open = true" @mouseleave="open = false"></div>
+                    <a href="{{ route('game.show', ['alias' => $game->alias]) }}">
+                        <div class="bg-cover bg-center w-12 h-12 rounded-md cursor-pointer"
+                            style="background-image: url('{{ asset('storage/' . $game->logo_url) }}');"
+                            @mouseover="open = true" @mouseleave="open = false"></div>
                     </a>
                 @else
                     <div class="w-12 h-12 border-dashed border-2 border-gray-300 rounded-md cursor-pointer"
@@ -31,11 +27,7 @@
 
                 <div x-show="open"
                     class="absolute -top-10 left-full transform -translate-x-1/2 p-2 text-sm text-content_text bg-background rounded shadow-lg whitespace-nowrap z-10 truncated-text320 whitespace-wrap max-w-[150px]">
-                    @if ($locale == 'en')
-                        {{ $game->name }}
-                    @else
-                        {{ $game->getTranslation('name', $locale) }}
-                    @endif
+                    {{ $game->name }}
                 </div>
             </div>
         @endforeach
