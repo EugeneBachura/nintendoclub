8542332b2835e5a7f0d1a951520783f80b6eb249
Eugene Bachura <eugenebachura@gmail.com>
2024-12-14 02:58:15 +0100

    Refactoring code

--- app/Http/Controllers/Auth/DiscordController.php
+++ app/Http/Controllers/Auth/DiscordController.php
@@ -23,11 +23,9 @@ class DiscordController extends \App\Http\Controllers\Controller
         $localUser = User::where('discord_id', $user->getId())->first();
 
         if (!$localUser) {
-            // Проверяем, существует ли пользователь с таким email
             $localUserByEmail = User::where('email', $user->getEmail())->first();
 
             if ($localUserByEmail) {
-                // Если пользователь с таким email существует, обновляем его discord_id и другие данные
                 $localUserByEmail->update([
                     'discord_id' => $user->getId(),
                     'name' => $user->getName(),
@@ -40,7 +38,6 @@ class DiscordController extends \App\Http\Controllers\Controller
                     'last_active_at' => now(),
                 ]);
             } else {
-                // Если пользователь с таким email не найден, создаем нового
                 $localUser = User::create([
                     'discord_id' => $user->getId(),
                     'name' => $user->getName(),
@@ -49,8 +46,7 @@ class DiscordController extends \App\Http\Controllers\Controller
                     'nickname' => $user->user['global_name'],
                     'password' => Hash::make(Str::random(16))
                 ]);
-                // Создаём новый профиль
-                // Проверяем старые данные пользователя
+
                 $oldUserData = OldUserData::where('discord_id', $user->getId())->first();
                 $experience = 0;
                 $coins = 0;
@@ -74,14 +70,13 @@ class DiscordController extends \App\Http\Controllers\Controller
                     'premium_points' => $premium_points,
                     'profile_description' => $profile_description,
                     'reputation_count' => 0,
-                    'daily_visits_count' => 0, // Поскольку пользователь только что зарегистрировался
-                    'last_active_at' => now() // Устанавливаем текущее время как последнее время активности
+                    'daily_visits_count' => 0,
+                    'last_active_at' => now()
                 ]);
                 $localUser->profile()->save($profile);
                 $localUser->assignRole('user');
             }
         } else {
-            // Если пользователь с таким discord_id уже существует, обновляем его имя и email
             $localUser->update([
                 'email' => $user->getEmail(),
                 'name' => $user->getName(),
@@ -89,7 +84,6 @@ class DiscordController extends \App\Http\Controllers\Controller
                 'avatar'   => $user->getAvatar(),
                 'nickname' => $user->user['global_name'],
             ]);
-            // Если пользователь существует, обновите его последнее время активности и увеличьте счетчик ежедневных посещений
             $localUser->profile->update([
                 'last_active_at' => now()
             ]);
