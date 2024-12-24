c3a0773b3c1a833ad8bacd339d827939ea774f4a
Eugene Bachura <eugenebachura@gmail.com>
2024-12-24 01:29:43 +0100

    Update localization files for Post Comments Moderation

---
+++ resources/lang/ru/texts.php
@@ -0,0 +1,14 @@
+<?php
+// resources/lang/ru/texts.php
+return [
+    'all_comments' => 'Все комментарии',
+    'pending_comments' => 'Комментарии на модерации',
+    'approved_comments' => 'Одобренные комментарии',
+    'rejected_comments' => 'Отклонённые комментарии',
+    'spam_comments' => 'Комментарии в спаме',
+    'no_comments_found' => 'Не найдено комментариев',
+    'by_user' => 'Пользователь',
+    'on_post' => 'Пост',
+    'posted_at' => 'Опубликовано',
+    'by_moderator' => 'Модератор'
+];
